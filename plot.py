333baa9f7330ad8206ec755992f5164556236078
mwasko5 <mwasko@arizona.edu>
2024-10-20 12:30:16 -0700

    plot update

--- plot.py
+++ plot.py
@@ -8,16 +8,16 @@ lambda_values = [100, 200, 300, 500, 700, 1000]
 def plot_csma_top_a():
     # Throughput A vs lambda
     plt.xlabel('λ (frames/second)')
-    plt.ylabel('throughput A')
+    plt.ylabel('throughput A (Kbps)')
 
     throughputs = []
     for i in range(len(lambda_values)):
-        throughputs += [(main.total_successA_dcf[0][i] * 12000) / 10]
+        throughputs += ([(main.total_successA_dcf[0][i] * 12000 / 1000) / 10]) # the divide by 1000 is to get Kbps
 
     plt.grid()
     plt.xticks([0, 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000])
     
-    plt.title('Throughput A vs. λ using CSMA (topology a)')
+    plt.title('Throughput A (Kbps) vs. λ using CSMA (topology a)')
 
     plt.plot(lambda_values, throughputs, 'o-') # plt.plot([x], [y])
     plt.show()
